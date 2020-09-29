5a24f26a933c86807874f71c51a85c7582070526
cperez1012 <perezchristian1012@gmail.com>
2020-09-29 17:25:00 -0400

    changed info within div for main container to MyLists

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MyLists from './MyLists.js';
 
 const MainContainer = () => {
     return (
         <div className="MainContainer">
-            I am the Main Container
+           <MyLists/>
         </div>
     )
 }
