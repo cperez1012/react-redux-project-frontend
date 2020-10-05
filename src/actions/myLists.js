637b53bf1b54c0b0a456f61180caa5723151827f
cperez1012 <perezchristian1012@gmail.com>
2020-10-05 09:31:35 -0400

    added action creators for adding list and creating list

--- src/actions/myLists.js
+++ src/actions/myLists.js
@@ -7,6 +7,13 @@ export const setMyLists = lists => {
     }
 }
 
+export const addList = list => {
+    return {
+        type: "ADD_LIST",
+        list
+    }
+}
+
 //asynchronous actions
 export const getMyLists = () => {
     return dispatch => {
@@ -33,5 +40,25 @@ export const getMyLists = () => {
 export const clearLists = () => {
     return {
         type: "CLEAR_LISTS"
+    }
+}
+
+export const createList = (listData) => {
+    return dispatch => {
+        const remitListData = {
+            title: listData.title
+        }
+
+    return fetch("http://localhost:3001/api/v1/lists", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(remitListData)
+    })
+        .then(response => response.json())
+        .then(console.log)
+        .catch(console.log)
     }
 }
