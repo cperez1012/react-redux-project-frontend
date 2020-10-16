e02e55505b003304f7a5a5f36fd15807bd608a79
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 18:31:52 -0400

    added sync actions updateListSuccess and deleteSuccess, updated fetch requests with userId for route path, added updateList, deleteList and dispatches with history and userId for createList, updateList, and deleteList

--- src/actions/myLists.js
+++ src/actions/myLists.js
@@ -1,6 +1,11 @@
+import { setCurrentUser } from './currentUser.js'
+import { resetListForm } from './newListForm.js'
+
 //synchronous actions
 
 export const setMyLists = lists => {
+
+    // debugger
     return {
         type: "SET_MY_LISTS",
         lists
@@ -14,22 +19,41 @@ export const addList = list => {
     }
 }
 
+export const updateListSuccess = list => {
+    return {
+        type: "UPDATE_LIST",
+        list
+    }
+}
+
+export const deleteListSuccess = listId => {
+    return {
+        type: "DELETE_LIST",
+        listId
+    }
+}
+
 //asynchronous actions
-export const getMyLists = () => {
+export const getMyLists = (userId) => {
     return dispatch => {
-        return fetch("http://localhost:3001/api/v1/lists", {
+        // debugger
+        // return fetch(`http://localhost:3001/api/v1/users/${user.id}/lists`, {
+            // debugger
+        return fetch(`http://localhost:3001/api/v1/users/${userId}/lists`, {
             credentials: "include",
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
             },
         })
-            .then(response => response.json())
+            .then(resp => resp.json())
             .then(response => {
                 if (response.error) {
                     alert(response.error)
                 } else {
                     console.log(response.data)
+                    // debugger
+                    // dispatch(setCurrentUser(response.data))
                     dispatch(setMyLists(response.data))
                 }
             })
@@ -43,13 +67,15 @@ export const clearLists = () => {
     }
 }
 
-export const createList = (listData) => {
+export const createList = ( listData, history, userId) => {
+    // debugger
     return dispatch => {
         const remitListData = {
             title: listData.title
         }
 
-    return fetch("http://localhost:3001/api/v1/lists", {
+    // return fetch(`http://localhost:3001/api/v1/users/${user.id}/lists`, {
+    return fetch( `http://localhost:3001/api/v1/users/${userId}/lists`, {
         credentials: "include",
         method: "POST",
         headers: {
@@ -57,8 +83,74 @@ export const createList = (listData) => {
         },
         body: JSON.stringify(remitListData)
     })
-        .then(response => response.json())
-        .then(console.log)
+        .then(resp => resp.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error) 
+                } else {
+                    // dispatch(setCurrentUser(response.data))
+                    // debugger
+                    dispatch(addList(response.data))
+                    dispatch(resetListForm())
+                    history.push(`/lists/${response.data.id}`)
+            }
+        })
+        // .then(console.log)
         .catch(console.log)
+    }
+}
+
+export const updateList = ( listData, history, userId) => {
+    // debugger
+    return dispatch => {
+
+        debugger
+        const remitListData = {
+            title: listData.title
+        }
+
+        debugger
+        return fetch( `http://localhost:3001/api/v1/users/${userId}/lists/${listData.listId}`,{
+            credentials: "include",
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(remitListData)
+        })
+            .then(resp => resp.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    // dispatch(setCurrentUser(response.data))
+                    dispatch(updateListSuccess(response.data))
+                    history.push(`/lists/${response.data.id}`)
+                }
+            })
+            .catch(console.log)
+    }
+}
+
+export const deleteList = ( listId, history, userId) => {
+    return dispatch => {
+        return fetch( `http://localhost:3001/api/v1/users/${userId}/lists/${listId}`, {
+            credentials: "include",
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => resp.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    dispatch(setCurrentUser(response.data))
+                    dispatch(deleteListSuccess(listId))
+                    history.push(`/lists`)
+                }
+            })
+            .catch(console.log)
     }
 }
