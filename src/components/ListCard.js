af7a9298265e1e0038ba6b5d5c333c767af6312f
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 22:47:18 -0400

    added edit List name link, added import Link from react-router-dom, work in progress on listing fighter associated to list names

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,9 +1,33 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ListCard = ({ list }) =>  {
+    // debugger
+    console.log(list)
+
+    // const Fighters = ({ fighter }) => {
+    //     console.log(fighter)
+    debugger
     return (
-    <p>{list.attributes.title}</p>
+        // list ?
+        <div>
+            <h1>{list.attributes.title}</h1>
+            <p>{list.relationships.fighters.data.map(({fighter}) => fighter)}</p>
+            <Link to={`/lists/${list.id}/edit`}>Edit This List Name</Link>
+        </div> 
+        // : (
+        // <p>This List Card has no lists!</p>
+        // )   
     )
 }
+
+// const mapStateToProps = (state) => {
+//     return {
+//       list: state
+//     }
+// }
+
+// export default connect(mapStateToProps)(ListCard);
 
 export default ListCard;
