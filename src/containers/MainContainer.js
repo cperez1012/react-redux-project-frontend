30b37976bb6d8a3f4a78b3d1ffc42f1257fcde54
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 22:31:52 -0400

    created container folder for MainContainer

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyLists from '../components/MyLists.js';
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+           <MyLists/>
+        </div>
+    )
+}
+
+export default MainContainer;
