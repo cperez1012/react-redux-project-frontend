91a236579a32320597a60ef44a3216b48e90045b
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 23:59:18 -0400

    added add lists, update list, and delete list

--- src/reducers/myLists.js
+++ src/reducers/myLists.js
@@ -12,6 +12,12 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case "SET_MY_LISTS":
             return action.lists
+        case "ADD_LISTS":
+            return state.concat(action.list)
+        case "UPDATE_LIST":
+            return state.map(list => list.id === action.list.id ? action.list : list)
+        case "DELETE_LIST":
+            return state.filter(list => list.id === action.listId ? false : true)
         case "CLEAR_LISTS":
             return initialState
         default:
