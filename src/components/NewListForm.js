4b2a28be6e52c2263ff35cf94acadce9547b10fa
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 23:26:59 -0400

    updated mapStateToProps with userId constant defined by the conditional statement using currentUser id, returns new list form and userId, added paramaters to NewListForm constant

--- src/components/NewListForm.js
+++ src/components/NewListForm.js
@@ -1,38 +1,53 @@
 import React from 'react';
+// 1.  VVV We first grab the action creator
 import { updateNewListForm } from '../actions/newListForm.js';
+// import { createList } from '../actions/myLists.js'
 import { connect } from 'react-redux';
 
-const NewListForm = ({ name }) => {
+// 3.  This means Redux gives us back a prop called updateNewTripForm
+// which when invoked, actually Redux will now dispatch
+const NewListForm = ({ formData, updateNewListForm, userId, list, handleSubmit, editMode }) => {
+
+    const { title } = formData
 
     const handleChange = event => {
         const { name, value } = event.target
+        // 4.  This is not an invocation of just the action creator,
+        // it's now Redux dispatching the action built by the action
+        // creator with the appropriate arguments
         updateNewListForm(name, value)
 
 
     }
 
-    const handleSubmit = event => event.preventDefault()
+    // const handleSubmit = event => event.preventDefault()
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={event => {
+            event.preventDefault()
+            handleSubmit(formData)
+        }}>
             <input 
                 placeholder="title"
                 name="title"
                 onChange={handleChange}
-                value={name}    
+                value={title}    
             /><br></br>
             <input 
                 type="submit"
-                value="Create List"
+                value={ editMode ? "Update List" : "Create List" }
             />
         </form>
-    )};
+)};
 
 const mapStateToProps = (state) => {
-    const { name } =state.newListForm
+    const userId = state.currentUser ? state.currentUser.id : ""
     return {
-        name
+        formData: state.newListForm,
+        userId
     }
 }
 
+// 2.  We pass the action creator to redux's connect function
+// using either mapDispatchToProps or the shorthand object syntax seen below.
 export default connect(mapStateToProps, { updateNewListForm })(NewListForm);
