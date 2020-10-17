50c5d8428437034d808dd06c855d212c89e83679
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 22:40:58 -0400

    added Welcome text to Home page under signup and log in link, uncommented Login and Signup import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Login from './Login.js';
-import Signup from './Signup.js';
+// import Login from './Login.js';
+// import Signup from './Signup.js';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
     <div>
         <span>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup">Sign Up </Link>
             or
-            <Link to="/login">Log In</Link>
+            <Link to="/login"> Log In</Link>
+            <h1>WELCOME TO THE BOXING FAN APP</h1>
+            <p>This application let's you create all the interesting fun lists you have regarding your favorite/least favorite fighters that are actively fighter or not!</p>
         </span>
     </div>
 );
