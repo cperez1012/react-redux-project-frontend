fcbbc90b43d8bb1504e5c9cada10933d98c8e9fe
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 22:38:05 -0400

    added sync actions resetListform and setFormDataForEdit

--- src/actions/newListForm.js
+++ src/actions/newListForm.js
@@ -1,8 +1,27 @@
 // sync action
 export const updateNewListForm = (name, value) => {
-    console.log(name, value)
+    // console.log(name, value)
+    const formData = { name, value }
+    debugger
     return {
         type: "UPDATE_NEW_LIST_FORM",
-        formData: { name, value }
+        formData
+    }
+}
+
+export const resetListForm = () => {
+    return {
+        type: "RESET_NEW_LIST_FORM",
+    }
+}
+
+export const setFormDataForEdit = list => {
+    debugger
+    const listFormData = {
+        title:  list.attributes.title
+    }
+    return {
+        type: "SET_FORM_DATA_FOR_EDIT",
+        listFormData
     }
 }
