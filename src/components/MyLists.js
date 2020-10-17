edec9ecfc3bb528bb7beed372f559b88aac8de5f
cperez1012 <perezchristian1012@gmail.com>
2020-10-16 23:18:10 -0400

    updated listCards constant with Link route to individual List title being displayed

--- src/components/MyLists.js
+++ src/components/MyLists.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import ListCard from './ListCard.js';
+// import ListCard from './ListCard.js';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const MyLists = props => {
+    // debugger
     const listCards = props.lists.length > 0 ?
-        props.lists.map(l => <ListCard list={l} key={l.id} />) :
+    
+props.lists.map(l => (<p key={l.id}><Link to={`/lists/${l.id}`}>{l.attributes.title}</Link></p>)) :
         null
-
+// debugger
     return (
         listCards
     )
 }
+
+// const MyLists = ({ match }) => (
+//     <div>
+//         <Route path={match.url + "/lists"} component={MyLists}/>
+//     </div>
+// )
 
 const mapStateToProps = state => {
     return {
