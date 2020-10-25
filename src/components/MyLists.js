90be58b99faa88efbc864d238d79ac75f16d9310
cperez1012 <perezchristian1012@gmail.com>
2020-10-25 14:06:54 -0400

    WIP, trying to show fighters under List title

--- src/components/MyLists.js
+++ src/components/MyLists.js
@@ -3,27 +3,45 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+// debugger
 const MyLists = props => {
-    // debugger
+    debugger
     const listCards = props.lists.length > 0 ?
     
-props.lists.map(l => (<p key={l.id}><Link to={`/lists/${l.id}`}>{l.attributes.title}</Link></p>)) :
+props.lists.map(l =>
+    (<ul> 
+<li key={l.id}><Link to={`/lists/${l.id}`}>{l.attributes.title}</Link>
+</li>
+
+{/* <ol>
+    {l.relaionships.fighters.data.included.map(f => 
+        (<li key={f.id}><Link to={`/fighters/${f.id}`}>
+            {f.attributes.name}
+        </Link>
+ 
+        </li>)
+        )}
+</ol> */}
+</ul>)) 
+// && props.lists.relationships.map(f => (
+// <li key={f.id}>{f.attributes.name}</li>)) 
+:
         null
-// debugger
+
+    // const fighterCards = props.fighters.map(f => (<p>{f.attributes.name}</p>))
+debugger
     return (
         listCards
+        // ,
+        // fighterCards
     )
 }
-
-// const MyLists = ({ match }) => (
-//     <div>
-//         <Route path={match.url + "/lists"} component={MyLists}/>
-//     </div>
-// )
 
 const mapStateToProps = state => {
     return {
         lists: state.myLists
+        ,
+        fighters: state.myFighters
     }
 }
 
