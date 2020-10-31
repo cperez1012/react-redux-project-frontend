de5af1284591cc0fb78d1ec72f3eece9201fb714
cperez1012 <perezchristian1012@gmail.com>
2020-10-31 12:26:24 -0400

    created fighters.js file wihtin reducers folder

---
+++ src/reducers/fighters.js
@@ -0,0 +1,29 @@
+const initialState = []
+
+export default (state=initialState, action) => {    
+    switch (action.type) {
+        case "SET_FIGHTERS":
+           return action.fighters
+    default:
+          return state  
+    }
+}
+    // const initialState = []
+
+    // export default (state = initialState, action) => {
+    //     switch (action.type) {
+    //         case "SET_MY_LISTS":
+    //             debugger
+    //             return action.lists
+    //         case "ADD_LISTS":
+    //             return state.concat(action.list)
+    //         case "UPDATE_LIST":
+    //             return state.map(list => list.id === action.list.id ? action.list : list)
+    //         case "DELETE_LIST":
+    //             return state.filter(list => list.id === action.listId ? false : true)
+    //         case "CLEAR_LISTS":
+    //             return initialState
+    //         default:
+    //             return state
+    //     }
+    // }
