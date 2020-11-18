504a464514a1f17d6d7120b8a01cf6c4004bbdf3
cperez1012 <perezchristian1012@gmail.com>
2020-11-17 21:57:52 -0500

    built out fighter card with fighter's information

--- src/components/FighterCard.js
+++ src/components/FighterCard.js
@@ -9,12 +9,43 @@ const FighterCard = ({ fighter }) =>  {
     return (
         <div>
             <h1>{fighter.attributes.name}</h1>
-            <Link to={`/fighters/${fighter.id}/edit`}>Edit This Fighter's Name</Link>
+            <ul key={fighter.id}>
+            <Link to={`/fighters/${fighter.id}/edit`}>Edit This Fighter</Link>
+                <li>
+                   <label>Alias: </label>
+                   <p>{fighter.attributes.alias}</p>
+                   <label>Nationality: </label>
+                   <p>{fighter.attributes.nationality}</p>
+                   <label>Division: </label>
+                   <p>{fighter.attributes.division}</p>
+                   <label>Stance: </label>
+                   <p>{fighter.attributes.stance}</p>
+                   <label>Heighte: </label>
+                   <p>{fighter.attributes.height}</p>
+                   <label>Reach: </label>
+                   <p>{fighter.attributes.reach}</p>
+                   <label>Status: </label>
+                   <p>{fighter.attributes.status}</p>
+                   <label>Champion: </label>
+                   <p>{fighter.attributes.champion}</p>
+                   <label>Win: </label>
+                   <p>{fighter.attributes.win}</p>
+                   <label>Loss: </label>
+                   <p>{fighter.attributes.loss}</p>
+                   <label>Draw: </label>
+                   <p>{fighter.attributes.draw}</p>
+                   <label>KO: </label>
+                   <p>{fighter.attributes.ko}</p>
+                   <label>List: </label>
+                   <p>{fighter.attributes.list.title}</p>
+                </li>
+            </ul> 
         </div>    
-    )
+    ) 
 }
 
 const mapStateToProps = state => {
+    debugger
     return {
         fighters: state.fighters
     }
