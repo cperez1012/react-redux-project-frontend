36657794677368c021b45c7ad27a49479f62378f
cperez1012 <perezchristian1012@gmail.com>
2020-11-20 15:42:28 -0500

    updated champion line with Ternary operator

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -13,20 +13,33 @@ const ListCard = ({ list }) =>  {
             <h1>{list.attributes.title}</h1>
             <Link to={`/lists/${list.id}/edit`}>Edit This List Name</Link>
             <ol>{list.attributes.fighters.map(fighter =>
-                <li>
-                   <p>{fighter.name}</p>
-                   <p>{fighter.alias}</p>
-                   <p>{fighter.nationality}</p>
-                   <p>{fighter.division}</p>
-                   <p>{fighter.stance}</p>
-                   <p>{fighter.height}</p>
-                   <p>{fighter.reach}</p>
-                   <p>{fighter.status}</p>
-                   <p>{fighter.champion}</p>
-                   <p>{fighter.win}</p>
-                   <p>{fighter.loss}</p>
-                   <p>{fighter.draw}</p>
-                   <p>{fighter.ko}</p>
+                <li key={fighter.id}>
+                    <label>Name: </label>
+                    <p>{fighter.name}</p>
+                    <label>Alias: </label>
+                    <p>{fighter.alias}</p>
+                    <label>Nationality: </label>
+                    <p>{fighter.nationality}</p>
+                    <label>Division: </label>
+                    <p>{fighter.division}</p>
+                    <label>Stance: </label>
+                    <p>{fighter.stance}</p>
+                    <label>Height: </label>
+                    <p>{fighter.height}</p>
+                    <label>Reach: </label>
+                    <p>{fighter.reach}</p>
+                    <label>Status: </label>
+                    <p>{fighter.status}</p>
+                    <label>Are they a champion? </label>
+                    <p>{fighter.champion ? "Yes" : "No"}</p>
+                    <label>W: </label>
+                    <p>{fighter.win}</p>
+                    <label>L: </label>
+                    <p>{fighter.loss}</p>
+                    <label>D: </label>
+                    <p>{fighter.draw}</p>
+                    <label>KO: </label>
+                    <p>{fighter.ko}</p>
 
                 </li> 
             )}</ol>
