6d54b5ce41b94b1662efce1b5281896e68feaf52
cperez1012 <perezchristian1012@gmail.com>
2021-04-06 23:08:54 -0400

    added spacing to Home page for better look

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 const Home = () => (
     <div>
         <span>
+            <br></br>
             <Link to="/signup">Sign Up </Link>
+            <br></br>
             or
+            <br></br>
             <Link to="/login"> Log In</Link>
+            <br></br>
+            <br></br>
             <h1>WELCOME TO THE BOXING FAN APP</h1>
+            <br></br>
             <p>This application let's you create all the interesting fun lists you have regarding your favorite/least favorite fighters that are actively fighter or not!</p>
+            <br></br>
         </span>
     </div>
 );
