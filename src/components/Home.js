deb7fba2dabf1868da576b20d7daa7c523ecbfaa
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:33:25 -0400

    changed button to bootstrap button styling and added spacing to text and header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/esm/Button';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
     <div>
         <span>
             <br></br>
-            <Link to="/signup">Sign Up </Link>
-            <br></br>
-            or
-            <br></br>
-            <Link to="/login"> Log In</Link>
-            <br></br>
             <br></br>
             <h1>WELCOME TO THE BOXING FAN APP</h1>
+            <Link to="/signup">
+                <Button>
+                    Signup
+                </Button>    
+            </Link>
+            {" or "}
+            <Link to="/login"> 
+                <Button>
+                    Login
+                </Button>
+            </Link>
             <br></br>
-            <p>This application let's you create all the interesting fun lists you have regarding your favorite/least favorite fighters that are actively fighter or not!</p>
+            <br></br>
+            <br></br>
+            <p>This app let's you create all the interesting fun lists you have regarding your favorite/least favorite fighters that are actively fighter or not!</p>
             <br></br>
         </span>
     </div>
