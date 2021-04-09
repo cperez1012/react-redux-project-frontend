f99cda62bfa62d9de08f741e65acc9b2f0870e12
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:19:31 -0400

    removed uncommented lines wihtin index.js

--- src/index.js
+++ src/index.js
@@ -4,33 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-// First import Redux
-// import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-// import thunk from "redux-thunk";
-// import userReducer from './reducers/users.js'
 
-// const reducer = combineReducers({
-//   // Key of user, pointed to the store within ./actions/user.js
-//   user: userReducer
-// })
-
-// Putting our middleware together
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 import store from './store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Router } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'; 
 
 ReactDOM.render(
-  // <React.StrictMode>
   <Provider store={ store }>
     <Router>
       <App />
     </Router>
   </Provider>,
-  // </React.StrictMode>,
   document.getElementById('root')
 );
 
