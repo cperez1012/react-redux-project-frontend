97b274dd84cd7779d5fee9bb9c41761617cb7879
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:31:20 -0400

    added Form, Grid, TextField, AccountCircle from material ui and bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateLoginForm } from '../actions/loginForm.js';
 import { login } from '../actions/currentUser.js';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/esm/Button';
 
 const Login = ({ loginForm, updateLoginForm, login, history }) => {
 
@@ -17,16 +22,43 @@ const Login = ({ loginForm, updateLoginForm, login, history }) => {
         const handleSubmit = event => {
             event.preventDefault()
             login(loginForm, history)
-        }
-    return (
+        } 
 
-        <form onSubmit = {handleSubmit}>
-            <input placeholder="Username" value ={loginForm.username} name= "username" type="text" onChange ={handleInputChange} />
-            <input placeholder="Password" value ={loginForm.password} name= "password" type="text" onChange = {handleInputChange} />
-            <input  value = "Log In" type="submit" />
-        </form>
+
+    return (
+        <div className="login">
+            <Form onSubmit = {handleSubmit}>
+                <Grid container spacing={1} alignItems="flex-end">
+                <Grid item>
+                    <AccountCircle />
+                </Grid>
+
+                <Grid item>
+                    <TextField id="input-with-icon-grid" label="Username" placeholder="Enter Your Username" value ={loginForm.username} name= "username" type="text" onChange ={handleInputChange}  />
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid item>
+                    <AccountCircle />
+                </Grid>
+                
+                <Grid item>
+                    <TextField id="input-with-icon-grid" label="Password" placeholder="Enter Your Password" value ={loginForm.password} name= "password" type="text" onChange = {handleInputChange} />
+                </Grid>
+                <br></br>
+                <br></br>
+                </Grid>
+                <br></br>
+                <Button type="submit" value="Login">
+                    Login
+                </Button>
+                
+            </Form>
+        </div>
     )
+
 }
+
 // It should take a first argument called state, optionally a second argument called ownProps, and return a plain object containing the data that the connected component needs.
 
 // This function should be passed as the first argument to connect, and will be called every time when the Redux store state changes
