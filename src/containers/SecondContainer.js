0cc787d3b949fd0fce144256e99e2c10f7ed769a
cperez1012 <perezchristian1012@gmail.com>
2021-04-08 20:20:43 -0400

    removed Upvote component from second container

--- src/containers/SecondContainer.js
+++ src/containers/SecondContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Fighters from '../components/Fighters.js';
-import Upvote from '../components/Upvote.js';
 
 const SecondContainer = () => {
     return (
         <div className="SecondContainer">
            <Fighters/>
-           <Upvote/>
         </div>
     )
 }
