e8baac65d42ecb90820f69835d1d5e007379958d
cperez1012 <perezchristian1012@gmail.com>
2021-04-11 00:54:44 -0400

    uncommented Upvote component

--- src/components/Upvote.js
+++ src/components/Upvote.js
@@ -1,31 +1,31 @@
 // Functional Component
 
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/esm/Button';
+// import React, { useState } from 'react';
+// import Button from 'react-bootstrap/esm/Button';
 
-const Upvote = () => {
-    const [count, setCount] = useState(0);
+// const Upvote = () => {
+//     const [count, setCount] = useState(0);
 
-    const increment = () => {
-        setCount(count + 1)
-    }
+//     const increment = () => {
+//         setCount(count + 1)
+//     }
 
-    const decrement = () => {
-        setCount(count - 1)
-    }
+//     const decrement = () => {
+//         setCount(count - 1)
+//     }
 
-    return (
-        <div>
-            <Button onClick={increment} disabled={count === 1}>
-                Upvote
-            </Button>
-            {" "}
-            <Button  onClick={decrement} disabled={count === -1}>
-                Downvote
-            </Button>
-            <div>{count}</div>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <Button onClick={increment} disabled={count === 1}>
+//                 Upvote
+//             </Button>
+//             {" "}
+//             <Button  onClick={decrement} disabled={count === -1}>
+//                 Downvote
+//             </Button>
+//             <div>{count}</div>
+//         </div>
+//     )
+// }
 
-export default Upvote;
+// export default Upvote;
