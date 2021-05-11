cc0109d8297d20733b17208958387edf85835e85
cperez1012 <perezchristian1012@gmail.com>
2021-05-10 22:34:36 -0400

    added save to local storage and persist to state in order to store information even when reloading the page

--- src/store.js
+++ src/store.js
@@ -8,6 +8,17 @@ import fighters from './reducers/fighters.js'
 import newFighterForm from './reducers/newFighterForm.js'
 import thunk from "redux-thunk";
 
+function saveToLocalStorage(state) {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+}
+
+function loadFromLocalStorage() {
+    const serializedState = localStorage.getItem('state');
+    if(serializedState === null) return undefined;
+        return JSON.parse(serializedState);
+}
+
 // displaying different syntax options here: lines 8 & 9
 const reducer = combineReducers({
     // Key of user, pointed to the store within ./actions/user.js
@@ -25,6 +36,9 @@ const reducer = combineReducers({
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+const persistedState = loadFromLocalStorage();
+const store = createStore(reducer, persistedState, composeEnhancer(applyMiddleware(thunk)));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
